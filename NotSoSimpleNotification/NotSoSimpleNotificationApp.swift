3ea5d1bea5021846b8f862a172eaf1e9bcc13d37
Parth Manaktala <manaktala.parth@gmail.com>
2024-08-19 19:44:50 -0400

    Initial Commit

---
+++ NotSoSimpleNotification/NotSoSimpleNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotSoSimpleNotificationApp.swift
+//  NotSoSimpleNotification
+//
+//  Created by Parth Manaktala on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NotSoSimpleNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
